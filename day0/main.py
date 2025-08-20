deecb53d44860385e80dc0ecd988d1a30c5f3ba0
Andy Leung <leungadh@gmail.com>
2025-08-20 10:15:12 +0800

    initial scaffold: env, deps, structure

---
+++ day0/main.py
@@ -0,0 +1,20 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "requests",
+#     "rich",
+# ]
+# ///
+import sys
+from rich import print
+import requests
+
+
+print(sys.version)
+
+
+
+
+
+
+
